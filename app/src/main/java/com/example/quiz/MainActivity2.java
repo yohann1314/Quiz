28fa8bcd29e2a0a5f940eda049ec4d9e7b2103b1
yohann1314 <vb.yohann@gmail.com>
2024-07-05 15:33:30 +0200

    Deuxieme Page

--- app/src/main/java/com/example/quiz/MainActivity2.java
+++ app/src/main/java/com/example/quiz/MainActivity2.java
@@ -1,6 +1,9 @@
 package com.example.quiz;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +16,7 @@ public class MainActivity2 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide(); // Cacher la barre d'action
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main2);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
@@ -20,5 +24,26 @@ public class MainActivity2 extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Button quizTheme1Button = findViewById(R.id.quizTheme1Button);
+        quizTheme1Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity2.this, Quiz1.class);
+                intent.putExtra("quizTheme", "Theme1"); // Passer le thème du quiz à QuizActivity
+                startActivity(intent);
+            }
+        });
+
+        Button quizTheme2Button = findViewById(R.id.quizTheme2Button);
+        quizTheme2Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity2.this, Quiz2.class);
+                intent.putExtra("quizTheme", "Theme2"); // Passer le thème du quiz à QuizActivity
+                startActivity(intent);
+            }
+        });
+
     }
 }
